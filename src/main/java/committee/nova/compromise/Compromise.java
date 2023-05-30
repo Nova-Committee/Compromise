fb8fda420487eb2029f1b067dc4f39d868762216
MikhailTapio <mikhailtapio13468@163.com>
2023-05-30 22:15:50 +0800

    Compatibility display fix

--- src/main/java/committee/nova/compromise/Compromise.java
+++ src/main/java/committee/nova/compromise/Compromise.java
@@ -1,13 +1,16 @@
 package committee.nova.compromise;
 
 import committee.nova.compromise.client.config.ClientConfig;
+import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
+import org.apache.commons.lang3.tuple.Pair;
 
 @Mod("compromise")
 public class Compromise {
     public Compromise() {
         ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, ClientConfig.CFG);
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> "", (s, l) -> true));
     }
 }
